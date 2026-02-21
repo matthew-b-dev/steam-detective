e6125865ebf890c32403a407764bd5c26363becc
matthew-b-dev <11641649+matthew-b-dev@users.noreply.github.com>
2026-02-21 17:46:19 -0500

    update footer

--- src/components/SteamDetectiveFooter.tsx
+++ src/components/SteamDetectiveFooter.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import toast from 'react-hot-toast';
 
 const SteamDetectiveFooter: React.FC = () => {
+  const copyEmail = () => {
+    navigator.clipboard.writeText('hello@steamdetective.wtf');
+    toast.success('Contact Email copied to clipboard!', {
+      position: 'bottom-center',
+    });
+  };
+
   return (
     <footer className='mt-8 text-xs text-gray-400 text-right space-y-1 pb-52'>
       <div>Not affiliated with Valve Corporation or Steam</div>
@@ -29,12 +37,12 @@ const SteamDetectiveFooter: React.FC = () => {
       </div>
       <div>
         Contact{' '}
-        <a
-          href='mailto:hello@steamdetective.wtf'
-          className='text-yellow-500 hover:text-yellow-400 underline'
+        <button
+          onClick={copyEmail}
+          className='text-yellow-500 hover:text-yellow-400 underline bg-transparent border-0 p-0 cursor-pointer outline-none'
         >
           hello@steamdetective.wtf
-        </a>
+        </button>
       </div>
     </footer>
   );
