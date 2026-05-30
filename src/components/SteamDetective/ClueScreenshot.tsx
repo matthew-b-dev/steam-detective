3a541ebd4792075d5bc25c6f29307af5d7c2c0b8
matthew-b-dev <11641649+matthew-b-dev@users.noreply.github.com>
2026-05-30 17:56:45 -0400

    attempt to fix letterboxing bug on IOS

--- src/components/SteamDetective/ClueScreenshot.tsx
+++ src/components/SteamDetective/ClueScreenshot.tsx
@@ -124,9 +124,10 @@ export const ClueScreenshot: React.FC<ClueScreenshotProps> = ({
                   }}
                   initial={{ filter: 'blur(10px)' }}
                   animate={{
-                    filter: isMobileViewport
-                      ? `blur(0px) brightness(${isBrightened ? 1.62 : 1.25})`
-                      : `blur(0px) brightness(${isBrightened ? 1.45 : 1})`,
+                    filter:
+                      isMobileViewport && !screenshotLetterbox
+                        ? `blur(0px) brightness(${isBrightened ? 1.62 : 1.25})`
+                        : `blur(0px) brightness(${isBrightened ? 1.45 : 1})`,
                   }}
                   exit={{ filter: 'blur(10px)' }}
                   transition={{ duration: 0.2 }}
